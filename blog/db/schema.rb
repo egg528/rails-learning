c951212e3682a0ea6aa3177617d64a8ffd7638ed
wkwon <rnjsdntjr26@daum.net>
2024-08-20 23:23:16 +0900

    feat: 신규 Model 생성 및 연관관계 설정 (Concern 활용)

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,12 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_19_124019) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_20_135939) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "published_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
